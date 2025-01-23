e9687571129750c778859583e2d66c54d6ee0e5f
ChickenLover <romangg81@gmail.com>
2025-01-23 13:17:18 +0200

    tmp

--- crates/prover/src/core/backend/icicle/column.rs
+++ crates/prover/src/core/backend/icicle/column.rs
@@ -1,28 +1,33 @@
 use std::iter::zip;
+use std::mem::transmute;
 use std::{array, mem};
 
 use bytemuck::allocation::cast_vec;
 use bytemuck::{cast_slice, cast_slice_mut, Zeroable};
+use icicle_core::vec_ops::{stwo_convert, transpose_matrix};
+use icicle_cuda_runtime::device_context::DeviceContext;
 use icicle_cuda_runtime::memory::{DeviceSlice, DeviceVec, HostOrDeviceSlice, HostSlice};
+use icicle_m31::field::{QuarticExtensionField, ScalarField};
 use itertools::{izip, Itertools};
 use num_traits::Zero;
 
 use super::IcicleBackend;
-use crate::core::backend::{Column, CpuBackend};
+use crate::core::backend::simd::column::SecureColumn;
+use crate::core::backend::{Column, ColumnOps, CpuBackend};
 use crate::core::fields::cm31::CM31;
 use crate::core::fields::m31::BaseField;
-use crate::core::fields::qm31::SecureField;
+use crate::core::fields::qm31::{SecureField, QM31};
 use crate::core::fields::secure_column::{SecureColumnByCoords, SECURE_EXTENSION_DEGREE};
 use crate::core::fields::{FieldExpOps, FieldOps};
 
 impl FieldOps<SecureField> for IcicleBackend {
-    fn batch_inverse(column: &DeviceSecureColumn, dst: &mut DeviceSecureColumn) {
+    fn batch_inverse(column: &DeviceColumn, dst: &mut DeviceColumn) {
         SecureField::batch_inverse(&column.data, &mut dst.data);
     }
 }
 
-impl<T: Debug + Clone + Default> ColumnOps<T> for IcicleBackend {
-    type Column = Vec<T>;
+impl<T: std::fmt::Debug + Clone + Default> ColumnOps<T> for IcicleBackend {
+    type Column = DeviceColumn;
 
     fn bit_reverse_column(column: &mut Self::Column) {
         // todo!()
@@ -31,6 +36,7 @@ impl<T: Debug + Clone + Default> ColumnOps<T> for IcicleBackend {
 }
 
 // A column that is stored on device
+#[derive(Clone, Debug, Default)]
 pub struct DeviceColumn {
     pub data: DeviceVec<BaseField>,
     /// The number of [`BaseField`]s in the vector.
@@ -48,33 +54,38 @@ impl DeviceColumn {
         &mut cast_slice_mut(&mut self.data)[..self.length]
     }
 
-    pub fn into_cpu_vec(mut self) -> Vec<BaseField> {
-        let capacity = self.data.capacity() * N_LANES;
-        let length = self.length;
-        let ptr = self.data.as_mut_ptr() as *mut BaseField;
-        let res = unsafe { Vec::from_raw_parts(ptr, length, capacity) };
-        mem::forget(self);
-        res
-    }
+    // pub fn into_cpu_vec(mut self) -> Vec<BaseField> {
+    //     unsafe {
+    //         let capacity = self.data.capacity() * N_LANES;
+    //         let length = self.length;
+    //         let ptr = self.data.as_mut_ptr() as *mut BaseField;
+    //         let res = unsafe { Vec::from_raw_parts(ptr, length, capacity) };
+    //         mem::forget(self);
+    //         res
+    //     }
+    // }
 
     pub fn from_cpu(values: Vec<BaseField>) -> Self {
         values.into_iter().collect()
     }
 
-    /// Returns a vector of `BaseColumnMutSlice`s, each mutably owning
-    /// `chunk_size` `PackedBaseField`s (i.e, `chuck_size` * `N_LANES` elements).
-    pub fn chunks_mut(&mut self, chunk_size: usize) -> Vec<BaseColumnMutSlice<'_>> {
-        self.data
-            .chunks_mut(chunk_size)
-            .map(BaseColumnMutSlice)
-            .collect_vec()
-    }
+    // /// Returns a vector of `BaseColumnMutSlice`s, each mutably owning
+    // /// `chunk_size` `PackedBaseField`s (i.e, `chuck_size` * `N_LANES` elements).
+    // pub fn chunks_mut(&mut self, chunk_size: usize) -> Vec<BaseColumnMutSlice<'_>> {
+    //     self.data
+    //         .chunks_mut(chunk_size)
+    //         .map(BaseColumnMutSlice)
+    //         .collect_vec()
+    // }
 
-    pub fn into_secure_column(self) -> SecureColumn {
-        let length = self.len();
-        let data = self.data.into_iter().map(PackedSecureField::from).collect();
-        SecureColumn { data, length }
-    }
+    // pub fn into_secure_column(self) -> SecureColumn {
+    //     let length = self.len();
+    //     unsafe {
+    //         let s_data =  transmute(std::slice::from_raw_parts_mut(self.data.as_mut_ptr(), length));
+    //         mem::forget(self);
+    //         SecureColumn { s_data, length }
+    //     }
+    // }
 }
 
 impl Column<BaseField> for DeviceColumn {
@@ -148,25 +159,26 @@ pub struct DeviceCM31Column {
 
 impl Column<CM31> for DeviceCM31Column {
     fn zeros(length: usize) -> Self {
-        Self {
-            data: vec![PackedCM31::zeroed(); length.div_ceil(N_LANES)],
-            length,
-        }
+        let mut data = DeviceVec::cuda_malloc(length).unwrap();
+
+        let host_data = vec![CM31::zero(); length];
+        data.copy_from_host(HostSlice::from_slice(&host_data));
+
+        Self { data, length }
     }
 
     #[allow(clippy::uninit_vec)]
     unsafe fn uninitialized(length: usize) -> Self {
-        let mut data = Vec::with_capacity(length.div_ceil(N_LANES));
-        data.set_len(length.div_ceil(N_LANES));
+        let mut data = DeviceVec::cuda_malloc(length).unwrap();
+
         Self { data, length }
     }
 
     fn to_cpu(&self) -> Vec<CM31> {
+        let mut host_data = Vec::<BaseField>::with_capacity(self.length);
         self.data
-            .iter()
-            .flat_map(|x| x.to_array())
-            .take(self.length)
-            .collect()
+            .copy_to_host(HostSlice::from_mut_slice(&mut host_data));
+        host_data
     }
 
     fn len(&self) -> usize {
@@ -174,13 +186,25 @@ impl Column<CM31> for DeviceCM31Column {
     }
 
     fn at(&self, index: usize) -> CM31 {
-        self.data[index / N_LANES].to_array()[index % N_LANES]
+        let mut host_vec = vec![CM31::zero(); 1];
+        unsafe {
+            DeviceSlice::from_slice(std::slice::from_raw_parts(self.data.as_ptr().add(index), 1))
+                .copy_to_host(HostSlice::from_mut_slice(&mut host_vec))
+                .unwrap();
+        }
+        host_vec[0]
     }
 
     fn set(&mut self, index: usize, value: CM31) {
-        let mut packed = self.data[index / N_LANES].to_array();
-        packed[index % N_LANES] = value;
-        self.data[index / N_LANES] = PackedCM31::from_array(packed)
+        let host_vec = vec![value; 1];
+        unsafe {
+            DeviceSlice::from_mut_slice(std::slice::from_raw_parts_mut(
+                self.data.as_mut_ptr().add(index),
+                1,
+            ))
+            .copy_from_host(HostSlice::from_slice(&host_vec))
+            .unwrap();
+        }
     }
 }
 
@@ -223,38 +247,40 @@ pub struct DeviceSecureColumn {
 impl DeviceSecureColumn {
     // Separates a single column of `PackedSecureField` elements into `SECURE_EXTENSION_DEGREE` many
     // `PackedBaseField` coordinate columns.
-    pub fn into_secure_column_by_coords(self) -> SecureColumnByCoords<SimdBackend> {
-        if self.len() < N_LANES {
-            return self.to_cpu().into_iter().collect();
-        }
+    // pub fn into_secure_column_by_coords(self) -> SecureColumnByCoords<SimdBackend> {
+    //     if self.len() < N_LANES {
+    //         return self.to_cpu().into_iter().collect();
+    //     }
 
-        let length = self.length;
-        let packed_length = self.data.len();
-        let mut columns = array::from_fn(|_| Vec::with_capacity(packed_length));
+    //     let length = self.length;
+    //     let packed_length = self.data.len();
+    //     let mut columns = array::from_fn(|_| Vec::with_capacity(packed_length));
 
-        for v in self.data {
-            let packed_coords = v.into_packed_m31s();
-            zip(&mut columns, packed_coords).for_each(|(col, packed_coord)| col.push(packed_coord));
-        }
+    //     for v in self.data {
+    //         let packed_coords = v.into_packed_m31s();
+    //         zip(&mut columns, packed_coords).for_each(|(col, packed_coord)| col.push(packed_coord));
+    //     }
 
-        SecureColumnByCoords {
-            columns: columns.map(|col| BaseColumn { data: col, length }),
-        }
-    }
+    //     SecureColumnByCoords {
+    //         columns: columns.map(|col| BaseColumn { data: col, length }),
+    //     }
+    // }
 }
 
 impl Column<SecureField> for DeviceSecureColumn {
     fn zeros(length: usize) -> Self {
-        Self {
-            data: vec![PackedSecureField::zeroed(); length.div_ceil(N_LANES)],
-            length,
-        }
+        let mut data = DeviceVec::cuda_malloc(length).unwrap();
+
+        let host_data = vec![SecureField::zero(); length];
+        data.copy_from_host(HostSlice::from_slice(&host_data));
+
+        Self { data, length }
     }
 
     #[allow(clippy::uninit_vec)]
     unsafe fn uninitialized(length: usize) -> Self {
-        let mut data = Vec::with_capacity(length.div_ceil(N_LANES));
-        data.set_len(length.div_ceil(N_LANES));
+        let mut data = DeviceVec::cuda_malloc(length).unwrap();
+
         Self { data, length }
     }
 
@@ -271,44 +297,56 @@ impl Column<SecureField> for DeviceSecureColumn {
     }
 
     fn at(&self, index: usize) -> SecureField {
-        self.data[index / N_LANES].to_array()[index % N_LANES]
+        let mut host_vec = vec![SecureField::zero(); 1];
+        unsafe {
+            DeviceSlice::from_slice(std::slice::from_raw_parts(self.data.as_ptr().add(index), 1))
+                .copy_to_host(HostSlice::from_mut_slice(&mut host_vec))
+                .unwrap();
+        }
+        host_vec[0]
     }
 
     fn set(&mut self, index: usize, value: SecureField) {
-        let mut packed = self.data[index / N_LANES].to_array();
-        packed[index % N_LANES] = value;
-        self.data[index / N_LANES] = PackedSecureField::from_array(packed)
-    }
-}
-
-impl FromIterator<SecureField> for DeviceSecureColumn {
-    fn from_iter<I: IntoIterator<Item = SecureField>>(iter: I) -> Self {
-        let mut chunks = iter.into_iter().array_chunks();
-        let mut data = (&mut chunks)
-            .map(PackedSecureField::from_array)
-            .collect_vec();
-        let mut length = data.len() * N_LANES;
-
-        if let Some(remainder) = chunks.into_remainder() {
-            if !remainder.is_empty() {
-                length += remainder.len();
-                let mut last = [SecureField::zero(); N_LANES];
-                last[..remainder.len()].copy_from_slice(remainder.as_slice());
-                data.push(PackedSecureField::from_array(last));
-            }
+        let host_vec = vec![value; 1];
+        unsafe {
+            DeviceSlice::from_mut_slice(std::slice::from_raw_parts_mut(
+                self.data.as_mut_ptr().add(index),
+                1,
+            ))
+            .copy_from_host(HostSlice::from_slice(&host_vec))
+            .unwrap();
         }
-
-        Self { data, length }
     }
 }
 
-impl FromIterator<PackedSecureField> for DeviceSecureColumn {
-    fn from_iter<I: IntoIterator<Item = PackedSecureField>>(iter: I) -> Self {
-        let data = iter.into_iter().collect_vec();
-        let length = data.len() * N_LANES;
-        Self { data, length }
-    }
-}
+// impl FromIterator<SecureField> for DeviceSecureColumn {
+//     fn from_iter<I: IntoIterator<Item = SecureField>>(iter: I) -> Self {
+//         let mut chunks = iter.into_iter().array_chunks();
+//         let mut data = (&mut chunks)
+//             .map(PackedSecureField::from_array)
+//             .collect_vec();
+//         let mut length = data.len() * N_LANES;
+
+//         if let Some(remainder) = chunks.into_remainder() {
+//             if !remainder.is_empty() {
+//                 length += remainder.len();
+//                 let mut last = [SecureField::zero(); N_LANES];
+//                 last[..remainder.len()].copy_from_slice(remainder.as_slice());
+//                 data.push(PackedSecureField::from_array(last));
+//             }
+//         }
+
+//         Self { data, length }
+//     }
+// }
+
+// impl FromIterator<PackedSecureField> for DeviceSecureColumn {
+//     fn from_iter<I: IntoIterator<Item = PackedSecureField>>(iter: I) -> Self {
+//         let data = iter.into_iter().collect_vec();
+//         let length = data.len() * N_LANES;
+//         Self { data, length }
+//     }
+// }
 
 // /// A mutable slice of a SecureColumnByCoords.
 // pub struct SecureColumnByCoordsMutSlice<'a>(pub [BaseColumnMutSlice<'a>;
@@ -344,76 +382,76 @@ impl FromIterator<PackedSecureField> for DeviceSecureColumn {
 // }
 
 impl SecureColumnByCoords<IcicleBackend> {
-    pub fn packed_len(&self) -> usize {
-        self.columns[0].data.len()
-    }
+    // pub fn packed_len(&self) -> usize {
+    //     self.columns[0].data.len()
+    // }
 
-    /// # Safety
-    ///
-    /// `vec_index` must be a valid index.
-    pub unsafe fn packed_at(&self, vec_index: usize) -> PackedSecureField {
-        PackedQM31([
-            PackedCM31([
-                *self.columns[0].data.get_unchecked(vec_index),
-                *self.columns[1].data.get_unchecked(vec_index),
-            ]),
-            PackedCM31([
-                *self.columns[2].data.get_unchecked(vec_index),
-                *self.columns[3].data.get_unchecked(vec_index),
-            ]),
-        ])
-    }
+    // /// # Safety
+    // ///
+    // /// `vec_index` must be a valid index.
+    // pub unsafe fn packed_at(&self, vec_index: usize) -> PackedSecureField {
+    //     PackedQM31([
+    //         PackedCM31([
+    //             *self.columns[0].data.get_unchecked(vec_index),
+    //             *self.columns[1].data.get_unchecked(vec_index),
+    //         ]),
+    //         PackedCM31([
+    //             *self.columns[2].data.get_unchecked(vec_index),
+    //             *self.columns[3].data.get_unchecked(vec_index),
+    //         ]),
+    //     ])
+    // }
 
-    /// # Safety
-    ///
-    /// `vec_index` must be a valid index.
-    pub unsafe fn set_packed(&mut self, vec_index: usize, value: PackedSecureField) {
-        let PackedQM31([PackedCM31([a, b]), PackedCM31([c, d])]) = value;
-        *self.columns[0].data.get_unchecked_mut(vec_index) = a;
-        *self.columns[1].data.get_unchecked_mut(vec_index) = b;
-        *self.columns[2].data.get_unchecked_mut(vec_index) = c;
-        *self.columns[3].data.get_unchecked_mut(vec_index) = d;
-    }
+    // /// # Safety
+    // ///
+    // /// `vec_index` must be a valid index.
+    // pub unsafe fn set_packed(&mut self, vec_index: usize, value: PackedSecureField) {
+    //     let PackedQM31([PackedCM31([a, b]), PackedCM31([c, d])]) = value;
+    //     *self.columns[0].data.get_unchecked_mut(vec_index) = a;
+    //     *self.columns[1].data.get_unchecked_mut(vec_index) = b;
+    //     *self.columns[2].data.get_unchecked_mut(vec_index) = c;
+    //     *self.columns[3].data.get_unchecked_mut(vec_index) = d;
+    // }
 
-    pub fn to_vec(&self) -> Vec<SecureField> {
-        izip!(
-            self.columns[0].to_cpu(),
-            self.columns[1].to_cpu(),
-            self.columns[2].to_cpu(),
-            self.columns[3].to_cpu(),
-        )
-        .map(|(a, b, c, d)| SecureField::from_m31_array([a, b, c, d]))
-        .collect()
-    }
+    // pub fn to_vec(&self) -> Vec<SecureField> {
+    //     izip!(
+    //         self.columns[0].to_cpu(),
+    //         self.columns[1].to_cpu(),
+    //         self.columns[2].to_cpu(),
+    //         self.columns[3].to_cpu(),
+    //     )
+    //     .map(|(a, b, c, d)| SecureField::from_m31_array([a, b, c, d]))
+    //     .collect()
+    // }
 
-    /// Returns a vector of `SecureColumnByCoordsMutSlice`s, each mutably owning
-    /// `SECURE_EXTENSION_DEGREE` slices of `chunk_size` `PackedBaseField`s
-    /// (i.e, `chuck_size` * `N_LANES` secure field elements, by coordinates).
-    pub fn chunks_mut(&mut self, chunk_size: usize) -> Vec<SecureColumnByCoordsMutSlice<'_>> {
-        let [a, b, c, d] = self
-            .columns
-            .get_many_mut([0, 1, 2, 3])
-            .unwrap()
-            .map(|x| x.chunks_mut(chunk_size));
-        izip!(a, b, c, d)
-            .map(|(a, b, c, d)| SecureColumnByCoordsMutSlice([a, b, c, d]))
-            .collect_vec()
-    }
+    // /// Returns a vector of `SecureColumnByCoordsMutSlice`s, each mutably owning
+    // /// `SECURE_EXTENSION_DEGREE` slices of `chunk_size` `PackedBaseField`s
+    // /// (i.e, `chuck_size` * `N_LANES` secure field elements, by coordinates).
+    // pub fn chunks_mut(&mut self, chunk_size: usize) -> Vec<SecureColumnByCoordsMutSlice<'_>> {
+    //     let [a, b, c, d] = self
+    //         .columns
+    //         .get_many_mut([0, 1, 2, 3])
+    //         .unwrap()
+    //         .map(|x| x.chunks_mut(chunk_size));
+    //     izip!(a, b, c, d)
+    //         .map(|(a, b, c, d)| SecureColumnByCoordsMutSlice([a, b, c, d]))
+    //         .collect_vec()
+    // }
 
-    pub fn from_cpu(cpu: SecureColumnByCoords<CpuBackend>) -> Self {
-        Self {
-            columns: cpu.columns.map(BaseColumn::from_cpu),
-        }
-    }
+    // pub fn from_cpu(cpu: SecureColumnByCoords<CpuBackend>) -> Self {
+    //     Self {
+    //         columns: cpu.columns.map(BaseColumn::from_cpu),
+    //     }
+    // }
 }
 
-impl FromIterator<SecureField> for SecureColumnByCoords<IcicleBackend> {
-    fn from_iter<I: IntoIterator<Item = SecureField>>(iter: I) -> Self {
-        let cpu_col = SecureColumnByCoords::<CpuBackend>::from_iter(iter);
-        let columns = cpu_col.columns.map(|col| col.into_iter().collect());
-        SecureColumnByCoords { columns }
-    }
-}
+// impl FromIterator<SecureField> for SecureColumnByCoords<IcicleBackend> {
+//     fn from_iter<I: IntoIterator<Item = SecureField>>(iter: I) -> Self {
+//         let cpu_col = SecureColumnByCoords::<CpuBackend>::from_iter(iter);
+//         let columns = cpu_col.columns.map(|col| col.into_iter().collect());
+//         SecureColumnByCoords { columns }
+//     }
+// }
 
 pub struct SecureColumnByCoordsIter<'a> {
     column: &'a SecureColumnByCoords<IcicleBackend>,
