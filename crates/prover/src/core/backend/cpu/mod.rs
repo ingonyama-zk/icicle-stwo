5914f9e3f20b6c407dd60407e9af04cd8991731f
Jeremy Felder <jeremy.felder1@gmail.com>
2025-02-03 13:58:58 +0200

    Remove cuda bit reverse from CPU backend

--- crates/prover/src/core/backend/cpu/mod.rs
+++ crates/prover/src/core/backend/cpu/mod.rs
@@ -38,54 +38,12 @@ impl BackendForChannel<Poseidon252MerkleChannel> for CpuBackend {}
 pub fn bit_reverse<T>(v: &mut [T]) {
     let n = v.len();
     assert!(n.is_power_of_two());
-    #[cfg(not(feature = "icicle"))]
-    {
-        let log_n = n.ilog2();
-        for i in 0..n {
-            let j = bit_reverse_index(i, log_n);
-            if j > i {
-                v.swap(i, j);
-            }
-        }
-    }
 
-    #[cfg(feature = "icicle")]
-    unsafe {
-        let limbs_count: usize = size_of_val(&v[0]) / 4;
-        use std::slice;
-
-        use icicle_core::traits::FieldImpl;
-        use icicle_core::vec_ops::{bit_reverse_inplace, BitReverseConfig, VecOps};
-        use icicle_cuda_runtime::device::get_device_from_pointer;
-        use icicle_cuda_runtime::memory::{DeviceSlice, HostSlice};
-        use icicle_m31::field::{ComplexExtensionField, QuarticExtensionField, ScalarField};
-
-        fn bit_rev_generic<T, F>(v: &mut [T], n: usize)
-        where
-            F: FieldImpl,
-            <F as FieldImpl>::Config: VecOps<F>,
-        {
-            let cfg = BitReverseConfig::default();
-
-            // Check if v is a DeviceSlice or some other slice type
-            let mut v_ptr = v.as_mut_ptr() as *mut F;
-            let rr = unsafe { slice::from_raw_parts_mut(v_ptr, n) };
-
-            // means data already on device (some finite device id, instead of huge number for host
-            // pointer)
-            if get_device_from_pointer(v_ptr as _).unwrap() <= 1024 {
-                bit_reverse_inplace(unsafe { DeviceSlice::from_mut_slice(rr) }, &cfg).unwrap();
-            } else {
-                bit_reverse_inplace(HostSlice::from_mut_slice(rr), &cfg).unwrap();
-            }
-        }
-
-        if limbs_count == 1 {
-            bit_rev_generic::<T, ScalarField>(v, n);
-        } else if limbs_count == 2 {
-            bit_rev_generic::<T, ComplexExtensionField>(v, n);
-        } else if limbs_count == 4 {
-            bit_rev_generic::<T, QuarticExtensionField>(v, n);
+    let log_n = n.ilog2();
+    for i in 0..n {
+        let j = bit_reverse_index(i, log_n);
+        if j > i {
+            v.swap(i, j);
         }
     }
 }
